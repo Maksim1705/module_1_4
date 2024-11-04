e07d76ce9fd4ef800934b315d03c98688dd56ebc
Максим <maksidrisoviska@gmail.com>
2024-11-05 01:58:44 +0300

    05.11

---
+++ module_1_4.py
@@ -0,0 +1,8 @@
+my_string=input("Из какого вы города?: ")
+print("Здраствуйте,мой город:," ,len(my_string))
+print('my_string'.upper())
+print('my_string'.lower())
+print('my string'.replace(' ', ''))
+name='my string'
+print(name[0])
+print(name[8])
